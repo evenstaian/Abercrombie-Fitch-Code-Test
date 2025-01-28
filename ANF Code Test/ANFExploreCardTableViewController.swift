995ec4349c9ad28f7acef64d836913691e564203
evenstaian <evenstaian@gmail.com>
2025-01-28 00:27:28 -0300

    creating PromotionCard UI using UIKit and ViewCode Pattern

--- ANF Code Test/ANFExploreCardTableViewController.swift
+++ ANF Code Test/ANFExploreCardTableViewController.swift
@@ -22,17 +22,31 @@ class ANFExploreCardTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = self.tableView.dequeueReusableCell(withIdentifier: "exploreContentCell", for: indexPath)
-        if let titleLabel = cell.viewWithTag(1) as? UILabel,
-           let titleText = exploreData?[indexPath.row]["title"] as? String {
-            titleLabel.text = titleText
-        }
         
-        if let imageView = cell.viewWithTag(2) as? UIImageView,
-           let name = exploreData?[indexPath.row]["backgroundImage"] as? String,
-           let image = UIImage(named: name) {
-            imageView.image = image
+        if let data = exploreData?[indexPath.row] {
+            if let titleLabel = cell.viewWithTag(1) as? UILabel,
+               let titleText = data["title"] as? String {
+                titleLabel.text = titleText
+            }
+            
+            if let imageView = cell.viewWithTag(2) as? UIImageView,
+               let name = data["backgroundImage"] as? String,
+               let image = UIImage(named: name) {
+                imageView.image = image
+            }
         }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        guard let data = exploreData?[indexPath.row] else { return }
+        
+        let promotionVC = PromotionCardViewController()
+        promotionVC.configure(with: data)
+        promotionVC.modalPresentationStyle = .fullScreen
+        present(promotionVC, animated: true)
     }
 }
---
+++ ANF Code Test/PromotionCardViewController.swift
@@ -0,0 +1,180 @@
+//
+//  PromotionCardViewController.swift
+//  ANF Code Test
+//
+//  Created by Evens Taian on 28/01/25.
+//
+
+import UIKit
+
+class PromotionCardViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private let backgroundImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let topDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 17)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let promoMessageLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 11)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let bottomDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let buttonsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.alignment = .center
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setupContrainsts()
+    }
+}
+
+extension PromotionCardViewController: ViewCode {
+    func setupViews() {
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        contentView.addSubview(backgroundImageView)
+        contentView.addSubview(topDescriptionLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(promoMessageLabel)
+        contentView.addSubview(buttonsStackView)
+        contentView.addSubview(bottomDescriptionLabel)
+    }
+    
+    func setupContrainsts() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            backgroundImageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            backgroundImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            
+            topDescriptionLabel.topAnchor.constraint(equalTo: backgroundImageView.bottomAnchor, constant: 16),
+            topDescriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            topDescriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            titleLabel.topAnchor.constraint(equalTo: topDescriptionLabel.bottomAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            promoMessageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            promoMessageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            promoMessageLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            buttonsStackView.topAnchor.constraint(equalTo: promoMessageLabel.bottomAnchor, constant: 16),
+            buttonsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            buttonsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            
+            bottomDescriptionLabel.topAnchor.constraint(equalTo: buttonsStackView.bottomAnchor, constant: 16),
+            bottomDescriptionLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            bottomDescriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            bottomDescriptionLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16)
+        ])
+    }
+}
+
+extension PromotionCardViewController {
+    func configure(with data: [AnyHashable: Any]) {
+        titleLabel.text = data["title"] as? String
+        topDescriptionLabel.text = data["topDescription"] as? String
+        promoMessageLabel.text = data["promoMessage"] as? String
+        bottomDescriptionLabel.text = data["bottomDescription"] as? String
+        
+        if let imageName = data["backgroundImage"] as? String {
+            backgroundImageView.image = UIImage(named: imageName)
+        }
+        
+        if let content = data["content"] as? [[AnyHashable: Any]] {
+            content.forEach { item in
+                let button = UIButton(type: .system)
+                button.titleLabel?.font = .systemFont(ofSize: 15)
+                button.setTitle(item["title"] as? String, for: .normal)
+                button.setTitleColor(.black, for: .normal)
+                button.backgroundColor = .white
+                button.layer.borderWidth = 1
+                button.layer.borderColor = UIColor.black.cgColor
+                button.layer.cornerRadius = 0
+                button.translatesAutoresizingMaskIntoConstraints = false
+                
+                if let target = item["target"] as? String,
+                   let url = URL(string: target) {
+                    button.tag = buttonsStackView.arrangedSubviews.count
+                    button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+                }
+                
+                buttonsStackView.addArrangedSubview(button)
+                
+                NSLayoutConstraint.activate([
+                    button.heightAnchor.constraint(equalToConstant: 44),
+                    button.widthAnchor.constraint(equalTo: buttonsStackView.widthAnchor, multiplier: 0.8)
+                ])
+            }
+        }
+    }
+    
+    @objc private func buttonTapped(_ sender: UIButton) {
+        // TODO
+    }
+}
