189fdd0919536ae3df95afaf9a9205ce2e4bb39b
evenstaian <evenstaian@gmail.com>
2025-01-28 00:37:38 -0300

    create models for JSON data structure mapping

---
+++ ANF Code Test/Models/PromoContent.swift
@@ -0,0 +1,13 @@
+//
+//  PromoContent.swift
+//  ANF Code Test
+//
+//  Created by Evens Taian on 28/01/25.
+//
+
+struct Content: Codable {
+    let elementType: String?
+    let target: String
+    let title: String
+}
+
